ef99c8f3c35d8889e4fc3379420982d0ef06dfdb
proku <andrejames1021@gmail.com>
2022-03-25 18:12:02 +0400

    .

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from scoreboard import Scoreboard
 from design import Design
 import time
 
+
 title_of_game = Design(text = "THE PONG GAME", font_size = 15, position = (0, -270))
 author = Design(text = "-- BY PROKU --", font_size = 7, position = (0, -280))
 
+
 game_is_on = True 
+
 
 # Making the screen.
 screen = Screen()
@@ -26,9 +29,11 @@ l_paddle = Paddle((-350, 0))
 # Making the ball object. 
 ball = Ball()
 
+
 # Making the objects for the scoreboard
 r_paddle_score = Scoreboard(position = (200, 240))
 l_paddle_score = Scoreboard(position = (-200, 240))
+
 
 # Keybindings (calling the function using self.function-name)
 screen.listen()
@@ -37,8 +42,10 @@ screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
+
 r_paddle_score = Scoreboard(position = (100, 240))
 l_paddle_score = Scoreboard(position = (-100, 240))
+
 
 while game_is_on:
     screen.update() 
@@ -72,9 +79,6 @@ while game_is_on:
 
 
 
-# Keep the Score
 
 
-
-# episode 202
 
